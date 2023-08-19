960b1a9742d2dd6718ff7ba5106e8a85e134141e
donlHenard <pastor.git101@gmail.com>
2023-08-19 23:14:20 +0800

    Testing the methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import * as date from 'date-fns';
+import {createTodo, createNote, createListItem} from './task';
+
+let sampleTodo1prop = {
+    title: "totle",
+    description: "asdasdasd",
+    dueDate: "12-12-12",
+    priority: 3
+};
+
+let sampleTodo1 = createTodo(sampleTodo1prop);
+console.log(sampleTodo1.getDescription());
+console.log(sampleTodo1.getTitle());
+sampleTodo1.setTitle("newTitle");
+console.log(sampleTodo1.getTitle());
