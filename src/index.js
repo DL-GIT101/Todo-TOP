d52e63fa6d3fd40866144b906730e9011524ecb6
donlHenard <pastor.git101@gmail.com>
2023-08-23 00:11:34 +0800

    Change the new names of methods

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import * as date from 'date-fns';
 import {createTodo,createNote,createListItem} from './task';
-import createCheckList from './container';
+import {createCheckList} from './container';
 
 let sugar = createListItem("sugar");
 let salt = createListItem("salt");
 let pepper = createListItem("get pepper");
-console.log(pepper.getDescription());
+console.log(pepper.getType());
 let toto = createTodo("title","asdasd","12-12-12",1);
 
-let shopping = createCheckList();
-shopping.addList(sugar);
-shopping.addList(salt);
-shopping.addList(pepper);
-console.log(shopping.getAllList());
+let shopping = createCheckList("checlist baby");
+shopping.addItem(sugar);
+shopping.addItem(salt);
+shopping.addItem(pepper);
+console.log(shopping.getAllItem());
 console.log(sugar.getState())
