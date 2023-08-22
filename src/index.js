f08f53156397fe2008793fabb643c6b76953e408
donlHenard <pastor.git101@gmail.com>
2023-08-22 17:04:17 +0800

    Add the factory fn and test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import * as date from 'date-fns';
-import {createTodo, createNote, createListItem} from './task';
+import {createTodo,createNote,createListItem} from './task';
 
-let sampleTodo1prop = {
-    title: "totle",
-    description: "asdasdasd",
-    dueDate: "12-12-12",
-    priority: 3
-};
+let todo = createTodo("title","asdasdad","12-12-12",2);
+console.log(todo.getTitle());
+todo.setTitle('new Title');
 
-let sampleTodo1 = createTodo(sampleTodo1prop);
-console.log(sampleTodo1.getDescription());
-console.log(sampleTodo1.getTitle());
-sampleTodo1.setTitle("newTitle");
-console.log(sampleTodo1.getTitle());
+let nota = createNote("asdasdasdasdasdasdasdasd");
+console.log(nota.getDescription());
+
+let list =  createListItem("asdasdasd");
+console.log(list.getComplete());
