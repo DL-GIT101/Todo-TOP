a34794e4cbd71d964d3789f61606aed5563867a4
donlHenard <pastor.git101@gmail.com>
2023-08-24 20:50:47 +0800

    Add createfooter fn

--- src/htmldoms.js
+++ src/htmldoms.js
@@ -1,9 +1,9 @@
+const body = document.querySelector("body");
+
 const createNavBar = (name, iconSrc) => {
 
-    const body = document.querySelector("body");
-
     const nav = document.createElement('nav');
-
+    nav.id = "navbar";
     const logoContainer = document.createElement('div');
 
     const logoName = document.createElement('h1');
@@ -12,7 +12,7 @@ const createNavBar = (name, iconSrc) => {
     const logo = document.createElement('object');
     logo.setAttribute("type", "image/svg+xml");
     logo.setAttribute("data", iconSrc);
-    logo.className = "icon";
+    logo.id = "icon";
 
     logoContainer.append(logoName,logo);
 
@@ -21,4 +21,25 @@ const createNavBar = (name, iconSrc) => {
 
 }
 
-export {createNavBar};
+const createFooter = (ghLogo, githubName) => {
+
+    const footer = document.createElement('nav');
+    footer.id = "footer";
+
+    const github = document.createElement('div');
+    github.className ="github";
+    const githubLogo = document.createElement('object');
+    githubLogo.setAttribute("type", "image/svg+xml");
+    githubLogo.setAttribute("data", ghLogo);
+    githubLogo.id = "ghLogo";
+
+    const ghUser = document.createElement('h5');
+    ghUser.innerHTML = githubName;
+
+    github.append(githubLogo,ghUser);
+    footer.append(github);
+    body.appendChild(footer);
+   
+}
+
+export {createNavBar,createFooter};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css';
-import { createNavBar } from './htmldoms';
+import { createNavBar,createFooter } from './htmldoms';
 import icon from './img/icon.svg';
+import githubLogo from './img/github.svg';
 import * as date from 'date-fns';
 import {createTodo,createNote,createListItem} from './task';
 import {createCheckList} from './container';
 
 createNavBar("TODO", icon);
+createFooter(githubLogo, "DL-GIT101");
