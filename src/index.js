21598ee65cdd00d33fad517056356ea104d6951d
donlHenard <pastor.git101@gmail.com>
2023-08-24 17:44:36 +0800

    Imported the htmldoms js and icon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import './style.css';
+import { createNavBar } from './htmldoms';
+import icon from './img/icon.svg';
 import * as date from 'date-fns';
 import {createTodo,createNote,createListItem} from './task';
 import {createCheckList} from './container';
 
-let sugar = createListItem("sugar");
-let salt = createListItem("salt");
-let pepper = createListItem("get pepper");
-console.log(pepper.getType());
-let toto = createTodo("title","asdasd","12-12-12",1);
-
-let shopping = createCheckList("checlist baby");
-shopping.addItem(sugar);
-shopping.addItem(salt);
-shopping.addItem(pepper);
-console.log(shopping.getAllItem());
-console.log(sugar.getState())
+createNavBar("TODO", icon);
