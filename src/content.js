143f2284f85824eb8e4220a81217dc2ca7cde212
donlHenard <pastor.git101@gmail.com>
2023-08-30 19:15:00 +0800

    Change sidebar and container for main

--- src/content.js
+++ src/content.js
@@ -1,27 +1,3 @@
-const createSidebar = () => {
-    
-    const sidebar = document.createElement('div');
-    sidebar.id ="sidebar";
-
-    const today = createButton("today","date");
-    const tommorrow = createButton("tommorrow","date");
-    const week = createButton("week","date");
-
-    const dates = createBtnGroup(today,tommorrow,week);
-
-    const title = document.createElement('h3');
-    title.innerHTML = "Project";
-    title.className = "title";
-
-    const other = createButton("others","project");
-
-    const project = createBtnGroup(title,other);
-
-    sidebar.append(dates,project);
-
-    return sidebar;
-}
-
 const createButton = (string,className) => {
 
     const button  = document.createElement('button');
@@ -41,4 +17,4 @@ const createBtnGroup = (...buttons) => {
     return container;
 }
 
-export {createSidebar};
+export {};
