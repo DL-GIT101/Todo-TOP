27b1b0a7093a864d637d79fc84270926421aae29
donlHenard <pastor.git101@gmail.com>
2023-09-06 21:01:14 +0800

    New syntax for todo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import githubLogo from './img/github.svg';
 import {addToLayout,createNavbar,createMain,createFooter, createSidebar, createMainContainer, removeAllChild} from './layout';
 import { displayProjectForm, displayProjectList } from './sidebar';
 import { createProject, createProjectList } from './container';
+import { createTodo } from './task';
+import { displayTodoList } from './content';
 
 addToLayout(
     createNavbar(icon, 'todo', []),
@@ -20,5 +22,13 @@ window.objectStorage = {
 
 for (let index = 1; index <= 5; index++) {
    createProject(index);
+   
 }
+
+for (let i = 0; i < objectStorage.projects.length; i++) {
+    for (let j = 0; j < 6; j++) {
+        objectStorage.projects[i].addItem(createTodo("sample","12-12-2023","low",i));
+    }
+}
+
 displayProjectList("projects",objectStorage);
