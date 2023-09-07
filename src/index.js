41417f046124ddc42dc4e0463cb894a6222427ba
donlHenard <pastor.git101@gmail.com>
2023-09-07 14:53:16 +0800

    Remove the not used fn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './style.css';
 import icon from './img/icon.svg';
 import githubLogo from './img/github.svg';
-import {addToLayout,createNavbar,createMain,createFooter, createSidebar, createMainContainer, removeAllChild} from './layout';
-import { displayProjectForm, displayProjectList } from './sidebar';
-import { createProject, createProjectList } from './container';
+import {addToLayout,createNavbar,createMain,createFooter, createSidebar, createMainContainer} from './layout';
+import { displayProjectList } from './sidebar';
+import { createProject } from './container';
 import { createTodo } from './task';
-import { displayTodoList } from './content';
 
 addToLayout(
     createNavbar(icon, 'todo', []),
