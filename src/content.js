f5f731c34979308f649325eef3ee9474ab5ae0c2
donlHenard <pastor.git101@gmail.com>
2023-09-07 20:47:02 +0800

    Add button for adding todo

--- src/content.js
+++ src/content.js
@@ -1,3 +1,4 @@
+import { createButton } from "./dom";
 import { removeAllChild,addToContent } from "./layout";
 
 const displayTodo = (text) => {
@@ -9,9 +10,15 @@ const displayTodo = (text) => {
     return container;
 }
 
+const addTodoButton = () => {
+    const button = createButton("add","+ add new todo");
+    return button;
+}
+
 const displayTodoList = (project) => {
     removeAllChild("container");
     let projectArray = project.getAllItem();
     projectArray.map(todo =>  addToContent(displayTodo(todo.getDescription())));
+    addToContent(addTodoButton());
 }
 export {displayTodoList};
