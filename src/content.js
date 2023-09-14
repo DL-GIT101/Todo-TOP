e081b77f8aac5465dd8103261bcb34b683114204
donlHenard <pastor.git101@gmail.com>
2023-09-14 17:24:19 +0800

    Add fn
    display form and adding todo
    on form will add more for date and priority input
    and adding todo will be just the same as project

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
 import { createButton } from "./dom";
-import { removeAllChild,addToContent } from "./layout";
+import { addToLayout, addToContent, removeAllChild } from "./layout";
 
 const displayTodo = (text) => {
 
@@ -10,15 +10,68 @@ const displayTodo = (text) => {
     return container;
 }
 
-const addTodoButton = () => {
-    const button = createButton("add","+ add new todo");
+const createAddTodoBtn = (text) => {
+    const button = createButton("add",text);
+    button.addEventListener('click',() => {
+        addToLayout(displayTodoForm());
+    })
     return button;
+}
+
+const displayTodoForm = () => {
+
+    const modal = document.createElement('div');
+    modal.className = "modal";
+    modal.id = "todo-form";
+
+    const content = document.createElement('div');
+    content.className = "content";
+
+    const form = document.createElement('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        addTodo();
+    })
+
+    const descriptionLabel = document.createElement('label');
+    descriptionLabel.textContent = "TODO";
+    descriptionLabel.setAttribute("for","description");
+
+    const descriptionInput = document.createElement('input');
+    descriptionInput.type = 'text';
+    descriptionInput.name = descriptionInput.id = descriptionInput.placeholder = "description";
+    descriptionInput.required = true;
+    descriptionInput.minLength = "2";
+    descriptionInput.maxLength = "100";
+
+    const buttonGroup = document.createElement('span');
+    buttonGroup.className = "group";
+
+    const addButton = createButton("add","add");
+    addButton.type = "submit";
+
+    const closeButton = createButton("close","close");
+    closeButton.type = "button";
+    closeButton.addEventListener('click', () => {
+        modal.remove();
+    })
+
+    buttonGroup.append(addButton,closeButton);
+    form.append(descriptionLabel,descriptionInput,buttonGroup);
+    content.appendChild(form);
+    modal.appendChild(content);
+    return modal;
 }
 
 const displayTodoList = (project) => {
     removeAllChild("container");
     let projectArray = project.getAllItem();
     projectArray.map(todo =>  addToContent(displayTodo(todo.getDescription())));
-    addToContent(addTodoButton());
+    addToContent(createAddTodoBtn("+ add new todo"));
 }
+
+const addTodo = () => {
+    alert("added todo");
+}
+
 export {displayTodoList};
